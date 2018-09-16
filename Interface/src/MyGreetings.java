9f1ef9d10b83ba9ab9fb0da8e23a977f85369e31
csemozid <csemozid@gmail.com>
2018-09-17 01:45:26 +0600

    Interface folder is added

---
+++ Interface/src/MyGreetings.java
@@ -0,0 +1,4 @@
+
+public interface MyGreetings {
+    public String getName(String name);
+}
---
+++ Interface/src/NumericRunTest.java
@@ -0,0 +1,8 @@
+
+public class NumericRunTest {
+
+	public static void main(String[] args){
+		 MyGreetings greeting = (name)-> "Hello "+name;
+		System.out.println(greeting.getName("A.Mozid"));
+	}
+}
